dd4587fe71fc11c8adac5a307b10a0972711b16b
Takashi SASAKI <takashi316@gmail.com>
2015-12-02 18:20:26 +0900

    set screen orientation and dim setings

--- Detector/src/main/java/com/gmail/takashi316/detector/DetectorActivity.java
+++ Detector/src/main/java/com/gmail/takashi316/detector/DetectorActivity.java
@@ -1,19 +1,24 @@
 package com.gmail.takashi316.detector;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 public class DetectorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         setContentView(R.layout.activity_detector);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
