397cb8e3a248c941ad285e2b62db03b41266c117
Takashi SASAKI <takashi316@gmail.com>
2015-12-02 22:45:01 +0900

    create CameraSurfaceView and SurfaceHolderCallback

---
+++ Detector/src/main/java/com/gmail/takashi316/detector/CameraSurfaceView.java
@@ -0,0 +1,13 @@
+package com.gmail.takashi316.detector;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by sasaki on 2015/12/02.
+ */
+public class CameraSurfaceView extends SurfaceView {
+    public CameraSurfaceView(Context context) {
+        super(context);
+    }
+}
---
+++ Detector/src/main/java/com/gmail/takashi316/detector/SurfaceHolderCallback.java
@@ -0,0 +1,23 @@
+package com.gmail.takashi316.detector;
+
+import android.view.SurfaceHolder;
+
+/**
+ * Created by sasaki on 2015/12/02.
+ */
+public class SurfaceHolderCallback implements SurfaceHolder.Callback {
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
