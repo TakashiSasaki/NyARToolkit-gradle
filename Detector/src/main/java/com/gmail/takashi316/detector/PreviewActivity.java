36f62143a2871d682b4f6263c25a81adbd111284
Takashi SASAKI <takashi316@gmail.com>
2015-12-03 21:24:35 +0900

    fix aspect ratio to 1:1

--- Detector/src/main/java/com/gmail/takashi316/detector/PreviewActivity.java
+++ Detector/src/main/java/com/gmail/takashi316/detector/PreviewActivity.java
@@ -1,10 +1,12 @@
 package com.gmail.takashi316.detector;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 /**
  * Created by sasaki on 2015/12/02.
@@ -19,7 +21,7 @@ public class PreviewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_preview);
         this.surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
     }
@@ -28,10 +30,10 @@ public class PreviewActivity extends Activity {
     protected void onStart() {
         super.onStart();
         camera = Camera.open();
-        this.surfaceHolderCallback = new SurfaceHolderCallback();
+        this.surfaceHolderCallback = new SurfaceHolderCallback(this);
         this.surfaceHolderCallback.setCamera(camera);
         this.surfaceView.getHolder().addCallback(this.surfaceHolderCallback);
-        this.surfaceView.setLayoutParams(this.surfaceHolderCallback.getLayoutParams());
+        this.surfaceView.setLayoutParams(new FrameLayout.LayoutParams(surfaceHolderCallback.getPreviewWidth(), surfaceHolderCallback.getPreviewHeight()));
     }
 
     @Override
--- Detector/src/main/java/com/gmail/takashi316/detector/SurfaceHolderCallback.java
+++ Detector/src/main/java/com/gmail/takashi316/detector/SurfaceHolderCallback.java
@@ -1,8 +1,11 @@
 package com.gmail.takashi316.detector;
 
+import android.content.Context;
 import android.hardware.Camera;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.SurfaceHolder;
-import android.widget.FrameLayout;
+import android.view.WindowManager;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,28 +16,42 @@ import java.util.List;
 public class SurfaceHolderCallback implements SurfaceHolder.Callback {
 
     private Camera camera;
-    private int maxHeight = 0, maxWidth = 0;
 
-    public SurfaceHolderCallback() {
+    public int getPreviewHeight() {
+        return previewHeight;
+    }
+
+    public int getPreviewWidth() {
+        return previewWidth;
+    }
+
+    private int previewHeight = 0, previewWidth = 0;
+    private int screenWidth, screenHeight;
+
+    public SurfaceHolderCallback(Context context) {
+        final WindowManager window_manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        final Display display = window_manager.getDefaultDisplay();
+        final DisplayMetrics display_metrics = new DisplayMetrics();
+        display.getMetrics(display_metrics);
+        screenHeight = display_metrics.heightPixels;
+        screenWidth = display_metrics.widthPixels;
     }
 
     public void setCamera(Camera camera) {
         this.camera = camera;
-    }
-
-    private void getMaxPreviewSize() {
+        if (this.camera == null) return;
         List<Camera.Size> sizes = this.camera.getParameters().getSupportedPreviewSizes();
+        this.previewHeight = 0;
+        this.previewWidth = 0;
         for (Camera.Size size : sizes) {
-            if (maxHeight < size.height) {
-                maxHeight = size.height;
-                maxWidth = size.width;
+            if (previewWidth > size.width || previewHeight > size.height) {
+                continue;
+            }
+            if (size.height <= screenHeight && size.width <= screenWidth) {
+                previewHeight = size.height;
+                previewWidth = size.width;
             }
         }
-    }
-
-    FrameLayout.LayoutParams getLayoutParams() {
-        this.getMaxPreviewSize();
-        return new FrameLayout.LayoutParams(maxWidth, maxHeight);
     }
 
     @Override
