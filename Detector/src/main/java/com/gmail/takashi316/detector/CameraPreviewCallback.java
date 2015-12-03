f1ee240561c0643eb539682803aca67780746d73
Takashi SASAKI <takashi316@gmail.com>
2015-12-03 22:50:26 +0900

    call NyARSensor#update in CameraPreviewCallback#onPreviewFrame

--- Detector/src/main/java/com/gmail/takashi316/detector/CameraPreviewCallback.java
+++ Detector/src/main/java/com/gmail/takashi316/detector/CameraPreviewCallback.java
@@ -3,6 +3,8 @@ package com.gmail.takashi316.detector;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 
+import jp.androidgroup.nyartoolkit.utils.NyARAndYUV420GsRaster;
+import jp.androidgroup.nyartoolkit.utils.NyARAndYUV420RgbRaster;
 import jp.nyatla.nyartoolkit.core.NyARException;
 import jp.nyatla.nyartoolkit.core.types.NyARIntSize;
 import jp.nyatla.nyartoolkit.markersystem.NyARSensor;
@@ -14,11 +16,18 @@ public class CameraPreviewCallback extends NyARSensor implements Camera.PreviewC
 
     byte[][] buffer;
     int bufferIndex;
+    private NyARAndYUV420RgbRaster _rgb_raster;
 
     public CameraPreviewCallback(int preview_width, int preview_height) throws NyARException {
         super(new NyARIntSize(preview_width, preview_height));
         this.buffer = new byte[2][preview_width * preview_height * ImageFormat.getBitsPerPixel(ImageFormat.NV21) / 8];
         this.bufferIndex = 1;
+        this._rgb_raster = new NyARAndYUV420RgbRaster(preview_width, preview_height);
+    }
+
+    @Override
+    protected void initResource(NyARIntSize s) throws NyARException {
+        this._gs_raster = new NyARAndYUV420GsRaster(s.w, s.h);
     }
 
     public byte[] getCurrentBuffer() {
@@ -29,5 +38,19 @@ public class CameraPreviewCallback extends NyARSensor implements Camera.PreviewC
     public void onPreviewFrame(byte[] data, Camera camera) {
         this.bufferIndex = (this.bufferIndex + 1) % 2;
         camera.addCallbackBuffer(this.buffer[this.bufferIndex]);
-    }
-}
+        synchronized (this) {
+            try {
+                this._rgb_raster.wrapBuffer(data);
+                this._gs_raster.wrapBuffer(data);
+                this.update(_rgb_raster);
+            } catch (NyARException e) {
+                e.printStackTrace();
+            }
+        }//synchronized
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }//onPreviewFrame
+}//CameraPreviewCallback
