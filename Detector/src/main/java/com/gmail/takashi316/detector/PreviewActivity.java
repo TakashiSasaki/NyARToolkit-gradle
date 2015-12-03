3909999292b742167cd24d877108777b26b4815d
Takashi SASAKI <takashi316@gmail.com>
2015-12-03 19:49:43 +0900

    release camera when activity stopping

--- Detector/src/main/java/com/gmail/takashi316/detector/PreviewActivity.java
+++ Detector/src/main/java/com/gmail/takashi316/detector/PreviewActivity.java
@@ -11,15 +11,31 @@ import android.widget.FrameLayout;
  * Created by sasaki on 2015/12/02.
  */
 public class PreviewActivity extends Activity {
+
+    Camera camera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_preview);
-        Camera camera = Camera.open();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        camera = Camera.open();
         CameraSurfaceView camera_surface_view = new CameraSurfaceView(this, camera);
         FrameLayout frame_layout = (FrameLayout) findViewById(R.id.frameLayout);
         frame_layout.addView(camera_surface_view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        camera.stopPreview();
+        camera.release();
+        camera = null;
     }
 }
